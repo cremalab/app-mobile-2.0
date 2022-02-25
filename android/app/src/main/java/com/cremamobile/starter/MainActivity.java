58143f91bcc52194e1aba9c9ffe56cbadd79e41d
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-02-25 10:47:19 -0600

    rename successful, app running

--- android/app/src/main/java/com/cremamobile/starter/MainActivity.java
+++ android/app/src/main/java/com/cremamobile/starter/MainActivity.java
@@ -1,4 +1,6 @@
-package com.appmobile20;
+package com.cremamobile.starter;
+import android.content.res.Configuration;
+import android.content.Intent;
 
 import android.os.Build;
 import android.os.Bundle;
@@ -9,7 +11,18 @@ import com.facebook.react.ReactRootView;
 
 import expo.modules.ReactActivityDelegateWrapper;
 
+
 public class MainActivity extends ReactActivity {
+
+    // Added automatically by Expo Config
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Intent intent = new Intent("onConfigurationChanged");
+        intent.putExtra("newConfig", newConfig);
+        sendBroadcast(intent);
+    }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     // Set the theme to AppTheme BEFORE onCreate to support 
--- android/app/src/main/java/com/cremamobile/starter/MainApplication.java
+++ android/app/src/main/java/com/cremamobile/starter/MainApplication.java
@@ -1,4 +1,4 @@
-package com.appmobile20;
+package com.cremamobile.starter;
 
 import android.app.Application;
 import android.content.Context;
@@ -79,7 +79,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.appmobile20.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.cremamobile.starter.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
