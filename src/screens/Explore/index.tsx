41212cb11b4e95463e444c9632d9fcd8439b4e14
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-02-24 22:17:58 -0600

    adjusting bottom margin based on header height

--- src/screens/Explore/index.tsx
+++ src/screens/Explore/index.tsx
@@ -1,11 +1,20 @@
+import { useHeaderHeight } from "@react-navigation/elements"
 import { useRoute } from "@react-navigation/native"
 import { Box, Text } from "native-base"
 
 export default function ExploreScreen() {
   const { name } = useRoute()
 
+  const headerHeight = useHeaderHeight()
+
   return (
-    <Box safeArea justifyContent="center" alignItems="center" flex="1">
+    <Box
+      mb={headerHeight}
+      safeArea
+      justifyContent="center"
+      alignItems="center"
+      flex="1"
+    >
       <Text variant="title3" color="primary.300">
         {name}
       </Text>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,33 @@
+import { useHeaderHeight } from "@react-navigation/elements"
 import { useNavigation, useRoute } from "@react-navigation/native"
 import { Box, Button, Text } from "native-base"
+import CremaLogo from "../../assets/CremaLogo.svg"
 import { RootNavigationProp } from "../../types/Navigation"
 
 export default function HomeScreen() {
   const { navigate } = useNavigation<RootNavigationProp>()
 
+  const headerHeight = useHeaderHeight()
+
   const { name } = useRoute()
 
-  const handleOpenPress = () => {
+  const handleOpenModal = () => {
     // navigating and passing params
     navigate("Modal", { message: "Hello World" })
   }
 
   return (
-    <Box safeArea justifyContent="center" alignItems="center" flex="1" mx="6">
+    <Box
+      safeArea
+      justifyContent="center"
+      alignItems="center"
+      flex="1"
+      mx="6"
+      mb={headerHeight}
+    >
+      <Box position="absolute" top="24">
+        <CremaLogo />
+      </Box>
       <Text mb="8" variant="title3" color="primary.300">
         {name}
       </Text>
@@ -21,7 +35,7 @@ export default function HomeScreen() {
         This is the home screen. Open up your text editor and replace this text
         to start creating your first app!
       </Text>
-      <Button mt="8" onPress={handleOpenPress}>
+      <Button mt="8" onPress={handleOpenModal}>
         Open Modal
       </Button>
     </Box>
