28b429350e07c82b6ad2a27247dd11b9ceebf05a
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-02-27 00:08:37 -0600

    conditional rendering for storybook

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import "react-native-gesture-handler"
+import { RENDER_STORYBOOK } from "@env"
 import { registerRootComponent } from "expo"
 import App from "./App"
+import StorybookUI from "./storybook"
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App)
+registerRootComponent(RENDER_STORYBOOK === "true" ? StorybookUI : App)
