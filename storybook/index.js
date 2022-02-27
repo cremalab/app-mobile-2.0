3d0fbecac1298012ac7df7b60ac89bde48e618a9
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-02-27 00:11:07 -0600

    storybook config

---
+++ storybook/index.js
@@ -0,0 +1,40 @@
+// if you use expo remove this line
+import { withKnobs } from "@storybook/addon-knobs"
+import {
+  addDecorator,
+  configure,
+  getStorybookUI,
+} from "@storybook/react-native"
+import { NativeBaseProvider } from "native-base"
+import React from "react"
+import { AppRegistry } from "react-native"
+import { customTheme } from "../src/theme"
+import { loadStories } from "./storyLoader"
+import "./rn-addons"
+
+// enables knobs for all stories
+addDecorator(withKnobs)
+
+// import stories
+configure(() => {
+  loadStories()
+}, module)
+
+// Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
+// To find allowed options for getStorybookUI
+const StorybookUI = getStorybookUI({
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  asyncStorage: require("@react-native-async-storage/async-storage").default,
+})
+
+const StorybookUIRoot = () => (
+  <NativeBaseProvider theme={customTheme}>
+    <StorybookUI />
+  </NativeBaseProvider>
+)
+
+// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
+// If you use Expo you should remove this line.
+AppRegistry.registerComponent("Crema Mobile Starter", () => StorybookUIRoot)
+
+export default StorybookUIRoot
---
+++ storybook/storyLoader.js
@@ -0,0 +1,25 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require("../src/screens/Explore/stories")
+  require("../src/screens/Home/stories")
+  require("../src/screens/Modal/stories")
+  require("../src/screens/Profile/stories")
+  require("../src/screens/Settings/stories")
+}
+
+const stories = [
+  "../src/screens/Explore/stories",
+  "../src/screens/Home/stories",
+  "../src/screens/Modal/stories",
+  "../src/screens/Profile/stories",
+  "../src/screens/Settings/stories",
+]
+
+module.exports = {
+  loadStories,
+  stories,
+}
