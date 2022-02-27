b9c3efabf13df1551e9e987683269c9ee3f1b46b
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-02-27 08:12:41 -0600

    logging out current NODE_ENV

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import "react-native-gesture-handler"
-import { RENDER_STORYBOOK } from "@env"
+import { NODE_ENV, RENDER_STORYBOOK } from "@env"
 import { registerRootComponent } from "expo"
 import App from "./App"
 import StorybookUI from "./storybook"
+
+if (NODE_ENV !== "production") {
+  console.log("Your current environment is", NODE_ENV)
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
