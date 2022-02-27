80ffd397237882d3f339870a4f38bb88cdb03775
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-02-27 00:09:28 -0600

    refactored test providers

--- src/testing/TestProvider.tsx
+++ src/testing/TestProvider.tsx
@@ -1,33 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import {
-  NavigationContainer,
-  ParamListBase,
-  RouteProp,
-} from "@react-navigation/native"
-import { createStackNavigator } from "@react-navigation/stack"
 import { NativeBaseProvider } from "native-base"
 import { ReactNode } from "react"
 import { customTheme } from "../theme"
 
-interface Props<ParamList extends ParamListBase, Props = any> {
-  mockedScreen?: {
-    name: keyof ParamList
-    component: React.ComponentType<Props>
-    params?: RouteProp<ParamList, keyof ParamList>["params"]
-  }
+interface Props {
   children?: ReactNode
 }
 
-export default function TestProvider<ParamList extends ParamListBase>({
-  mockedScreen,
-  children,
-}: Props<ParamList>) {
-  const Stack = createStackNavigator<ParamList>()
-
-  if (mockedScreen && children) {
-    throw Error("You can only pass a screen or children, not both.")
-  }
-
+export default function TestProvider({ children }: Props) {
   return (
     <NativeBaseProvider
       initialWindowMetrics={{
@@ -36,19 +15,7 @@ export default function TestProvider<ParamList extends ParamListBase>({
       }}
       theme={customTheme}
     >
-      <NavigationContainer>
-        {mockedScreen ? (
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen
-              initialParams={mockedScreen.params}
-              name={mockedScreen.name}
-              component={mockedScreen.component}
-            />
-          </Stack.Navigator>
-        ) : (
-          <>{children}</>
-        )}
-      </NavigationContainer>
+      {children}
     </NativeBaseProvider>
   )
 }
